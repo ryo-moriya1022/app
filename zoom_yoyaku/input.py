f61ed3f1be3195bf6f917475e7d212cc0bbd75ff
1 <[regu7828@gmail.com]>
2023-07-08 22:04:24 +0900

    最新バージョン

---
+++ zoom_yoyaku/input.py
@@ -0,0 +1,20 @@
+import datetime
+import streamlit as st
+from time import sleep
+import webbrowser
+import streamlit as st 
+def webopen(urls):
+    webbrowser.open(urls)
+def times(dt :datetime.datetime,urls):
+    now_dt = datetime.datetime.now().replace(second=0)
+    if dt < now_dt:
+        st.stop()
+    else:
+        while True:
+            now_dt = datetime.datetime.now().replace(second=0)
+            print("1 =", now_dt)
+            print("2 =", dt)
+            if abs((now_dt - dt).total_seconds()) < 1:
+                webopen(urls)
+                break
+            sleep(1）
