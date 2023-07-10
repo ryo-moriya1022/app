13fd50f9a6ead4d34fb16ff622d6666f46604249
1 <[regu7828@gmail.com]>
2023-07-10 21:23:28 +0900

    タイムゾーン修正

--- zoom_yoyaku/output.py
+++ zoom_yoyaku/output.py
@@ -1,32 +1,45 @@
 import streamlit as st
 import datetime  as dt
+from syori import times
+
+st.set_page_config(
+    page_title="URL自動実行システム",
+    layout="wide", 
+    initial_sidebar_state="auto", 
+    menu_items={
+        'Get Help': 'https://www.google.com',
+        'Report a bug': "https://www.google.com"
+    }
+)
 #chenge str to datetime
-def chenge_to_dt(input_time, now_year, now_month, now_day):
+def chenge_to_dt(input_time, now_days):
     dt_input_time=(
-        str(now_year)+"-"
-        +str(now_month)+"-"
-        +str(now_day)+'-'
-        +input_time
+        now_days+"-"+input_time
     )
     dt_input_time=dt.datetime.strptime(
         dt_input_time,"%Y-%m-%d-%H:%M"
         )
-    dt_input_time=dt_input_time+dt.timedelta(hours=9)
     return dt_input_time
+
+def errors(dt_input_time,now_time_datezone):
+    if dt_input_time<=now_time_datezone:
+        st.error("過去の予約はできません")
 # now time get 
 now_time=dt.datetime.now()
 # chenge now time to timezone(japan)
 now_time_datezone=now_time+dt.timedelta(hours=9)
-# concat input time to year and day and month
-now_year=now_time.year
-now_month=now_time.month
-now_day=now_time.day
+
+st.title("URL自動実行システム")
+st.header("概要")
+st.write("このプログラムは1週間以内の任意の時間に任意のURLを作動させるシステムとなっています")
+st.header("URLの入力 曜日 時間の入力")
 urls=st.text_input('URL',key='urls')
 st.date_input(
     "date",
     min_value=now_time_datezone,
     max_value=now_time_datezone+dt.timedelta(days=14),
-    value=now_time_datezone
+    value=now_time_datezone,
+    key="date"
 )
 st.text_input(
     "input_date",
@@ -35,6 +48,18 @@ st.text_input(
 )
 dt_input_time=chenge_to_dt(
     st.session_state["times"],
-    now_year,now_month,now_day
+    str(st.session_state["date"]),
     )
+errors(dt_input_time,now_time_datezone)
 st.write(now_time_datezone,dt_input_time)
+
+st.header("予約")
+st.write("ボタンを押すことで予約ができます。")
+st.button('予約',on_click=times,args=(
+    dt_input_time,
+    st.session_state["urls"],
+    )
+    )
+st.write("予約の取り消しをします")
+if st.button("取消",key="tor"):
+    st.stop()
---
+++ zoom_yoyaku/syori.py
@@ -0,0 +1,18 @@
+import datetime as dt
+import streamlit as st
+from time import sleep
+import webbrowser
+import streamlit as st 
+def webopen(urls):
+    webbrowser.open(urls)
+def times(input_dt :dt.datetime,urls):
+    now_dt=dt.datetime.now()+dt.timedelta(hours=9)
+    if input_dt < now_dt:
+        st.stop()
+    else:
+        while True:
+            now_dt = dt.datetime.now()+dt.timedelta(hours=9)
+            if abs((now_dt - input_dt).total_seconds()) < 1:
+                webopen(urls)
+                break
+            sleep(1)
