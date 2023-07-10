72905f1033bf65e949f8069daaa8b101fca88501
1 <[regu7828@gmail.com]>
2023-07-10 21:54:41 +0900

    mon

--- zoom_yoyaku/syori.py
+++ zoom_yoyaku/syori.py
@@ -12,6 +12,6 @@ def times(input_dt :dt.datetime,urls):
             now_dt = dt.datetime.now()+dt.timedelta(hours=9)
             if abs((now_dt - input_dt).total_seconds()) < 1:
                 st.write(urls)
-                webbrowser.open(urls)
+                webbrowser.open(urls,new=1,autoraise=True)
                 break
             sleep(1)
